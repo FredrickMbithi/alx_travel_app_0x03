0c9430ecf01cff4a82de2309df50897690c7a71e
ghost <fredrickmbithi000@gmail.com>
2025-11-09 13:17:33 +0300

    Prepare for Render deployment

--- alx_travel_app/alx_travel_app/urls.py
+++ alx_travel_app/alx_travel_app/urls.py
@@ -4,29 +4,9 @@ URL configuration for alx_travel_app project.
 """
 
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.conf import settings
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="ALX Travel App API",
-        default_version='v1',
-        description="API documentation for listings, bookings, reviews, and payment integration.",
-        contact=openapi.Contact(email="support@travelapp.com"),
-        license=openapi.License(name="MIT License"),
-    ),
-    public=True,
-    permission_classes=[permissions.AllowAny],
-)
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('listings.urls')),
-    # Swagger / Redoc (public docs)
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
